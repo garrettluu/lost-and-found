aaa7b16a2a57aff800e45cc8bcdf09c69509ebef
Haocheng Li <49082800+Teggoon@users.noreply.github.com>
2019-10-24 16:39:53 -0700

    Basic request receiving template, no handling yet

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/student/query', function(req, res, next) {
+  //handle student query request
+});
+
+
+router.get('/staff/item/in', function(req, res, next) {
+  //handle staff entering in new item
+});
+
+router.get('/staff/item/out', function(req, res, next) {
+  //handle staff removing an item
+});
+
+router.get('/staff/item/update', function(req, res, next) {
+  //handle staff updating an item
+  
 });
 
 module.exports = router;
